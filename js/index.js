cdae4b8565447829d2db2ae4f8d62112dfb36bae
Cookkie92 <74307133+Cookkie92@users.noreply.github.com>
2022-11-04 17:12:41 +0100

    update

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 import { books } from "./components/array.js";
+const listContainer = document.querySelector(".list-result");
+function listItems() {
+  for (let i = 0; i < books.length; i++) {
+    console.log(books[i]);
+    listContainer.innerHTML += `
+    <div class="list-result">
+    ${books[i].title}
+    <button>
+    </div>
+    `;
+  }
+}
+listItems();
