ec97173e6023bb3a94afbea67a5bfabde2f62f05
Cookkie92 <74307133+Cookkie92@users.noreply.github.com>
2022-11-11 16:21:33 +0100

    update

--- js/index.js
+++ js/index.js
@@ -25,7 +25,7 @@ function createList() {
   listContainer.innerHTML = "";
   books.forEach(function (item) {
     listContainer.innerHTML += `
-    <li >${item.title}  <i id="${item.isbn}" href="#">Remove</i> </li>
+    <li >${item.title}  <i id="${item.isbn}" href="#">X</i> </li>
 
      `;
   });
